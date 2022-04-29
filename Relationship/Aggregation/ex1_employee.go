8c2575f5d51a8c47b1fc39c3258c6337d243e3fb
huavanthong <50081052+huavanthong@users.noreply.github.com>
2022-04-29 16:21:00 +0700

    [Constructor] Update relationship for Aggregation with the different constructor.

--- Relationship/Aggregation/ex1_employee.go
+++ Relationship/Aggregation/ex1_employee.go
@@ -22,19 +22,64 @@ type Employee struct {
 	address Address
 }
 
-func NewEmployee(id int, name string, address Address) Employee {
-	return Employee{
+/*
+*****************************************************************************
+	new employee by value
+*****************************************************************************
+*/
+func NewEmployee(id int, name string, address Address) *Employee {
+	e := Employee{
 		id:      id,
 		name:    name,
 		address: address,
 	}
+	fmt.Printf("Check address after NewEmployee %p\n", &e)
+
+	return &e
 }
 
+/*
+*****************************************************************************
+	new employee by pointer
+*****************************************************************************
+*/
+func NewEmployeeByPointer(id int, name string, address Address) *Employee {
+
+	e := new(Employee)
+	e.id = id
+	e.name = name
+	e.address = address
+	fmt.Printf("Check address after NewEmployeeByPointer %p\n", &e)
+
+	return e
+}
+
+/*
+*****************************************************************************
+	main
+*****************************************************************************
+*/
 func main() {
 	address := NewAddress("Hau Giang", "HCM", "70000")
+	fmt.Printf("Check address initialize %p\n", &address)
+
+	/******************************************************************************
+		create a object using constructors by value
+	******************************************************************************/
 	employee := NewEmployee(1, "Hua Van Thong", address)
-	fmt.Printf("ID: %d ,\nName: %s, \n AddressLine: %s, \n",
+	fmt.Printf("ID: %d ,\nName: %s, \n AddressLine: %s, Address of employee.address %p \n",
 		employee.id,
 		employee.name,
-		employee.address.addressLine)
+		employee.address.addressLine,
+		&employee.address)
+
+	/******************************************************************************
+		create a object using constructors by pointer
+	******************************************************************************/
+	employee2 := NewEmployeeByPointer(1, "Hua Van Thong", address)
+	fmt.Printf("ID: %d ,\nName: %s, \n AddressLine: %s, Address of employee.address %p\n",
+		employee2.id,
+		employee2.name,
+		employee2.address.addressLine,
+		&employee2.address)
 }
