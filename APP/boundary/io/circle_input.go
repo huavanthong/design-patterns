f1de8193d61e616a3e19ef09c98069c5d98c6eaf
hvthong <hvthong@cmcglobal.vn>
2023-03-09 14:22:22 +0700

    Restructure code for circle boundary.

---
+++ APP/boundary/io/circle_input.go
@@ -0,0 +1,6 @@
+package boundary_input_output
+
+// Ta có thể đặt file này ở file khác chẳng hạn circle_input.go
+type CircleInput struct {
+	Radius float64
+}
---
+++ APP/boundary/io/circle_output.go
@@ -0,0 +1,7 @@
+package boundary_input_output
+
+// Ta có thể đặt file này ở file khác chẳng hạn circle_output.go
+type CircleOutput struct {
+	Area      float64
+	Perimeter float64
+}
